77f73e945fd2d71f798726686763959f31727769
jeymsooo <jeymsooo@gmail.com>
2022-06-06 17:17:35 +0800

    baby

---
+++ ATM-Sytem/src/razonadobaby.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class razonadobaby {
+	
+	public static void main (String[] args) {
+		 Scanner get = new Scanner (System.in);
+		 
+		 int num1, num2, total;
+		 
+		 System.out.print("Enter number: ");
+		 	num1 = get.nextInt();
+		 System.out.print("Enter number: ");
+			num2 = get.nextInt();
+			
+		 total = num1 + num2;
+		 
+		 System.out.print(num1 + "+" + num2 + "=" + total + ", fuck you");	
+	}
+
+}
