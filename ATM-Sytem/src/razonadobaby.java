380b4a8a22ea58e38dbef0cd8c8d0b193864f410
jeymsooo <jeymsooo@gmail.com>
2022-06-06 17:32:10 +0800

    1

--- ATM-Sytem/src/razonadobaby.java
+++ ATM-Sytem/src/razonadobaby.java
@@ -14,7 +14,7 @@ public class razonadobaby {
 			
 		 total = num1 + num2;
 		 
-		 System.out.print(num1 + "+" + num2 + "=" + total + ", fuck you");	
+		 System.out.print(num1 + "+" + num2 + "=" + total + ", fuck you weirdo");	
 	}
 
 }
