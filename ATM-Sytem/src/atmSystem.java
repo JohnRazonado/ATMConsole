b08644587923d858148323bc20464a43872b537d
John Razonado <john.razonado@cvsu.edu.ph>
2022-06-07 18:13:58 +0800

    Added Withdraw, Deposit, and Checking Balance
    Added the Withdraw, deposit, and checking function with some tweaks and
    delays for the user to read it as soon as possible. Still on clutter
    mode.

--- ATM-Sytem/src/atmSystem.java
+++ ATM-Sytem/src/atmSystem.java
@@ -8,30 +8,30 @@ public class atmSystem {
  public static void main(String[] args) throws IOException, InterruptedException{
 		 /* TODO
 		  * MAIN Tasks
-		  * 	Sectioning of the system
-		  * 	Account of user
-		  * 	User input interface
-		  * 	Deposit money
-		  * 	Withdraw transfer
+		  * DONE	Sectioning of the system
+		  * DONE	Account of user
+		  * DONE	User input interface
+		  * DONE	Deposit money
+		  * DONE	Withdraw transfer
 		  * 	Transfer funds
-		  * 	Check account balance
+		  * DONE	Check account balance
 		  * 	End Session
-		  * 	Enter Selection
+		  * DONE	Enter Selection
 		  * 	Create and Account (optional) Or have it fix
-		  * Cleaning of the Code
+		  * Cleaning of the Code TODO
 		  * 	Arranging of the code (functioning adding classes)
 		  * 	Making it pretty (adding color to text, simple animation if possible)
 		  * 	Making it dynamic (acknowledging the resolution of the command line and font size) Possible but optional
 		  * */
 		 
 		 // There could probably a built-in account of some sort here.
-		 // Checking and Savings account
+		 //   1: Savings  and  2: Checking	account
+	 
+		 double savBal = 0.00; 		// Savings account balance
+		 double savRate = 0.25;		// Savings interest rate 
 		 
 		 double checkBal = 0.00; 	// Checking account balance
 		 double checkRate = 0.00; 	// Checking interest rate
-		 
-		 double savBal = 0.00; 		// Savings account balance
-		 double savRate = 0.25;		// Savings interest rate 
 		 // The interest rate is based on the common interest rate of Philippine Banks
 		 
 		 
@@ -39,34 +39,113 @@ public class atmSystem {
 		 boolean transact = true;
 		 
 		 // This clear the CMD console but don't work on different IDEs
+		 // Remove if irresponsive
 		 System.out.print("\033[H\033[2J");  
 		 System.out.flush();  
 		 
 		 while (transact) {
 			 System.out.print("\n\nATM Main Menu:\n\n"
-					 + "1. Withdraw Money \n"
-					 + "2. Deposit Money \n"
-					 + "3. Transfer Funds \n"
-					 + "4. Check Account Balance\n"
-					 + "5. End Session\n \n"
+					 + "1: Withdraw Money \n"
+					 + "2: Deposit Money \n"
+					 + "3: Transfer Funds \n"
+					 + "4: Check Account Balance\n"
+					 + "5: End Session\n \n"
 					 + "Enter selection: ");
 			 int select = Integer.parseInt(input.readLine());
 			 
+			// This clear the CMD console but don't work on different IDEs
+			// Remove if irresponsive
+			 System.out.print("\033[H\033[2J");  
+			 System.out.flush();  
+			 
+			 
 			 switch (select) {
 				case 1: { //Withdraw Function here
-					
+					System.out.println("\n\nWITHDRAW");
+					System.out.print("Select Account:\n\t1: Savings\n\t2: Checking\n Enter selection: ");
+					select = Integer.parseInt(input.readLine());
+					if(select == 1) { // Savings Account
+						System.out.print("\n\nCurrent Balance " + savBal + "\n"
+								+ "Enter the amount to withdraw:");
+						double withdraw = Double.parseDouble(input.readLine());
+						double tempHolder = savBal;
+						tempHolder -= withdraw;
+						if(tempHolder > 0) {
+							savBal -= withdraw;
+							System.out.println("\nNew Savings Balance: " + savBal);
+						} else {
+							System.err.println("\nInsufficient Balance");
+						}
+				
+					} else if (select == 2) { // Checking Account
+						System.out.print("\n\nCurrent Balance " + checkBal + "\n"
+								+ "Enter the amount to withdraw:");
+						double withdraw = Double.parseDouble(input.readLine());
+						double tempHolder = checkBal;
+						tempHolder -= withdraw;
+						if(tempHolder > 0) {
+							checkBal -= withdraw;
+							System.out.println("\nNew Checking Balance: " + checkBal);
+						} else {
+							System.err.println("\nInsufficient Balance");
+						}
+					} else {
+						System.err.println("Something's wrong sorry about that");
+					}
 					break;
 				}
+				
+				
 				case 2: { //Deposit Function here
-					
+					System.out.println("\n\nDEPOSIT");
+					System.out.print("\nSelect Account:\n\t1: Savings\n\t2: Checking\n Enter selection: ");
+					select = Integer.parseInt(input.readLine());
+					if (select == 1) {
+						System.out.print("\n\nCurrent Balance " + savBal + "\n"
+								+ "Enter the amount to deposit: ");
+						double deposit = Double.parseDouble(input.readLine());
+						savBal += deposit;
+						System.out.println("New Savings Balance: " + savBal);
+					} else if (select == 2) {
+						System.out.print("\n\nCurrent Balance " + checkBal + "\n"
+								+ "Enter the amount to deposit: ");
+						double deposit = Double.parseDouble(input.readLine());
+						checkBal += deposit;
+						System.out.println("New Checking Balance: " + checkBal);
+					} else {
+						System.err.println("Something's wrong sorry about that");
+					}
 					break;
 				}
-				case 3: { //Transfer Function here
+				
+				
+				case 3: { //Transfer from Savings to Checking and Vice versa
+					System.out.println("\n\nTRANSFER");
+					System.out.print("\nSelect Account:\n\t1: Savings\n\t2: Checking\n Enter selection: ");
+					select = Integer.parseInt(input.readLine());
+					if (select == 1) {
+						
+					} else if (select == 2) {
+						
+					} else {
+						System.err.println("Something's wrong sorry about that");
+					}
 					
 					break;
 				}
 				case 4: { //Balance Check Function here
-					
+					System.out.println("\n\nCHECK BALANCE");
+					System.out.print("\nSelect Account:\n\t1: Savings\n\t2: Checking\n\t3: Both Account\n Enter selection: ");
+					select = Integer.parseInt(input.readLine());
+					if (select == 1) {
+						System.out.println("\n\nCurrent Balance " + savBal);
+					} else if (select == 2) {
+						System.out.println("\n\nCurrent Balance " + checkBal);
+					} else if(select == 3) {
+						System.out.println("\n\nSavings Balance: " + savBal + "\nChecking Balance: " + checkBal);
+					} else {
+						System.err.println("Something's wrong sorry about that");
+					}
 					break;
 				}
 				case 5: { //Outright exit the program
@@ -77,10 +156,10 @@ public class atmSystem {
 				default:
 					//throw new IllegalArgumentException("Unexpected value: " + select);
 					System.err.println("Please Choose only what's on the menu\nusing its corresponding number.\n\n");
-					TimeUnit.MILLISECONDS.sleep(500);
 			 }
+			 TimeUnit.SECONDS.sleep(2); // Some pause for the user to see the text
 		 } 
-		 System.out.println("Thank you for trusting on us\n\n EXITING...");
+		 System.out.println("\n\nThank you for trusting on us\n\n EXITING...");
  }
 
  // Questionable part here
