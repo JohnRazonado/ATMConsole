5d4a8dd473c61abdea17884a0735e199c3128ad6
John Razonado <john.razonado@cvsu.edu.ph>
2022-06-07 12:42:31 +0800

    Added TODOs
    Added the needed TODOs on the main file to keep things in-check

--- ATM-Sytem/src/atmSystem.java
+++ ATM-Sytem/src/atmSystem.java
@@ -2,6 +2,24 @@ import java.io.IOException;
 
 public class atmSystem {
  public static void main(String[] args) throws IOException{
+	 /* TODO
+	  * MAIN Tasks
+	  * 	Sectioning of the system
+	  * 	User input interface
+	  * 	Deposit money
+	  * 	Withdraw transfer
+	  * 	Transfer funds
+	  * 	Check account balance
+	  * 	End Session
+	  * 	Enter Selection
+	  * 	Create and Account (optional) Or have it fix
+	  * Cleaning of the Code
+	  * 	Arranging of the code (functioning adding classes)
+	  * 	Making it pretty (adding color to text, simple animation if possible)
+	  * 	Making it dynamic (acknowledging the resolution of the command line and font size) Possible but optional
+	  * */
+	 
+	 
 	 
 }
 }
