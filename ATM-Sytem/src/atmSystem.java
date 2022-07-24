2fd78559857a92af5b18e952a2809d73fde316f7
Darkinx <razonado.john1@gmail.com>
2022-07-24 20:17:35 +0800

    Separated coloring methods
    Separated the colorConsole functions that colors the atmSystem to clean up the main code

--- ATM-Sytem/src/atmSystem.java
+++ ATM-Sytem/src/atmSystem.java
@@ -245,50 +245,6 @@ public class atmSystem {
 		return Double.valueOf(new DecimalFormat("#.##").format(number));
 	}
 	
-// use this to color the texts
-class colorConsole {
-	public static final String RESET = "\033[0m"; 
-    public static final String BLACK = "\033[0;30m";   // BLACK
-    public static final String RED = "\033[0;31m";     // RED
-    public static final String GREEN = "\033[0;32m";   // GREEN
-    public static final String YELLOW = "\033[0;33m";  // YELLOW
-    public static final String BLUE = "\033[0;34m";    // BLUE
-    public static final String PURPLE = "\033[0;35m";  // PURPLE
-    public static final String CYAN = "\033[0;36m";    // CYAN
-    public static final String WHITE = "\033[0;37m";   // WHITE
-    public static final String LIGHT_PURPLE = "\033[38;2;200;0;200m"; //LIGHT PURPLE
-    public static final String TEAL = "\033[38;2;0;225;221m"; //TEAL
-    public static final String ORANGE = "\033[38;2;225;153;0m"; //ORANGE
-    public static final String LIGHT_GREEN = "\033[38;2;136;255;0m"; //LIGHT GREEN
-    public static final String LIGHT_BLUE = "\033[38;2;120;172;255m"; //LIGHT BLUE
-    public static final String DARK_BLUE = "\033[38;2;72;0;255m"; //DARK BLUE
-    public static final String ROSY_PINK = "\033[38;2;255;0;162m"; //ROSY PINK
-    public static final String BROWN = "\033[38;2;135;82;62m"; //BROWN
-    public static final String FOREST_GREEN = "\033[38;2;62;135;81m"; //FOREST GREEN
-    public static final String BANANA_YELLOW = "\033[38;2;240;238;113m"; //BANANA YELLOW
-    public static final String DARK_RED = "\033[38;2;145;40;16m"; //DARK RED
-    public static final String LIGHT_PINK = "\033[38;2;255;153;240m"; //LIGHT PINK
-    public static final String BLACK_BOLD_BRIGHT = "\033[0;30m";   // BLACK
-    public static final String RED_BOLD_BRIGHT = "\033[0;31m";     // RED
-    public static final String GREEN_BOLD_BRIGHT = "\033[0;32m";   // GREEN
-    public static final String YELLOW_BOLD_BRIGHT = "\033[0;33m";  // YELLOW
-    public static final String BLUE_BOLD_BRIGHT = "\033[0;34m";    // BLUE
-    public static final String PURPLE_BOLD_BRIGHT = "\033[0;35m";  // PURPLE
-    public static final String CYAN_BOLD_BRIGHT = "\033[0;36m";    // CYAN
-    public static final String WHITE_BOLD_BRIGHT = "\033[0;37m";   // WHITE
-    public static final String LIGHT_PURPLE_BOLD_BRIGHT = "\033[38;2;200;0;200m"; //LIGHT PURPLE
-    public static final String TEAL_BOLD_BRIGHT = "\033[38;2;0;225;221m"; //TEAL
-    public static final String ORANGE_BOLD_BRIGHT = "\033[38;2;225;153;0m"; //ORANGE
-    public static final String LIGHT_GREEN_BOLD_BRIGHT = "\033[38;2;136;255;0m"; //LIGHT GREEN
-    public static final String LIGHT_BLUE_BOLD_BRIGHT = "\033[38;2;120;172;255m"; //LIGHT BLUE
-    public static final String DARK_BLUE_BOLD_BRIGHT = "\033[38;2;72;0;255m"; //DARK BLUE
-    public static final String ROSY_PINK_BOLD_BRIGHT = "\033[38;2;255;0;162m"; //ROSY PINK
-    public static final String BROWN_BOLD_BRIGHT = "\033[38;2;135;82;62m"; //BROWN
-    public static final String FOREST_GREEN_BOLD_BRIGHT = "\033[38;2;62;135;81m"; //FOREST GREEN
-    public static final String BANANA_YELLOW_BOLD_BRIGHT = "\033[38;2;240;238;113m"; //BANANA YELLOW
-    public static final String DARK_RED_BOLD_BRIGHT = "\033[38;2;145;40;16m"; //DARK RED
-    public static final String LIGHT_PINK_BOLD_BRIGHT = "\033[38;2;255;153;240m"; //LIGHT PINK
- 	}
 }
 	
 
