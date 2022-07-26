d610da10cda7057bcb41afaf4ffbed5521c54472
Darkinx <razonado.john1@gmail.com>
2022-07-26 17:48:21 +0800

    Added TextSelect.java
    This will separate and remove the handling of the different transactions from the main java file while making it in an OPP way

---
+++ ATM-Sytem/src/TextSelect.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TextSelect{
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    Operation calc = new Operation();
+    // Operation accounts = new Operation();
+
+    public void select(String transaction, Account acc) throws IOException{
+        System.out.print(colorConsole.GREEN_BOLD_BRIGHT +"\nSelect Account:\n\t1: Savings\n\t2: Checking\n\nEnter selection: ");
+		int select = 0;
+        try {
+		    select = Integer.parseInt(input.readLine());
+		} catch (NumberFormatException e) {
+			System.out.println("Numberformat Error: Put numbers only");
+		} catch (IOException e) {
+			System.out.println("IO Error");
+		}
+        if (select == 1){
+            double balance = acc.getSaving();
+            balance = transact("Savings", transaction, balance);
+            acc.setSaving(balance);
+        } else if (select == 2){
+            double balance = acc.getChecking();
+            balance = transact("Checking", transaction, balance);
+            acc.setChecking(balance);
+        } else {
+            System.out.println(colorConsole.WHITE_BOLD_BRIGHT
+            + "-------------------------------------------");
+			System.err.println("\nChoose only based on the selection and its corresponding number");
+        }
+    }
+
+    // Will check if it is working
+    public double transact (String account, String transaction, double balance) throws NumberFormatException, IOException {
+        // account = Savings or Checking
+        // transaction = deposit or withdraw
+        
+
+        System.out.println(colorConsole.WHITE_BOLD_BRIGHT +"-------------------------------------------");
+			System.out.print(colorConsole.LIGHT_PINK_BOLD_BRIGHT +"\nCurrent Balance: " + "P" + (calc.formatter(balance)) + "\n\n" + "Enter the amount to "+ transaction + ": ");
+        double amount = Double.parseDouble(input.readLine());
+        if (transaction.equals("withdraw")) {
+            double tempHolder = balance;
+            tempHolder = calc.add(tempHolder, -amount);
+            if (tempHolder > 0) {
+                balance = calc.add(balance, -amount);
+            } else {
+                System.err.println("\nInsufficient Balance");
+            }
+        } else if(transaction.equals("deposit")){
+            balance += amount;
+        }
+        balance(transaction, balance);
+        return balance;
+    }
+    public void balance (String transaction,double balance){
+        System.out.println(colorConsole.LIGHT_GREEN_BOLD_BRIGHT +"\nNew "+ transaction +" Balance: " + "P" + (calc.formatter(balance)) + "\n");
+    }
+
+}
